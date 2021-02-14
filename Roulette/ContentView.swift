2ac8460b81be6058efb9251ac57ef9f21f0d166a
Petros <psdeme@hotmail.com>
2021-02-14 23:42:08 +0200

    - fixes and improvements

--- Roulette/ContentView.swift
+++ Roulette/ContentView.swift
@@ -6,60 +6,110 @@
 //
 
 import SwiftUI
-
+enum Color: String {
+    case red = "RED"
+    case black = "BLACK"
+    case green = "ZERO"
+    case empty
+}
+struct Sector: Equatable {
+    let number: Int
+    let color: Color
+}
 struct ContentView: View {
     @State private var isAnimating = false
-    @State private var degreesAnimation = 0.0
+    @State private var spinDegrees = 0.0
     @State private var rand = 0.0
     @State private var newAngle = 0.0
-    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     let halfSector = 360.0 / 37.0 / 2.0
-    let sectors: [String] = ["32 RED","15 BLACK","19 RED","4 BLACK","21 RED","2 BLACK","25 RED","17 BLACK","34 RED","6 BLACK","27 RED","13 BLACK","36 RED","11 BLACK","30 RED","8 BLACK","23 RED","10 BLACK","5 RED","24 BLACK","16 RED","33 BLACK","1 RED","20 BLACK","14 RED","31 BLACK","9 RED","22 BLACK","18 RED","29 BLACK","7 RED","28 BLACK","12 RED","35 BLACK","3 RED","26 BLACK","ZERO"]
-    var foreverAnimation: Animation {
-            Animation.easeOut(duration: 3.0)
-                .repeatCount(1, autoreverses: false)
-        }
-        
+    let sectors: [Sector] = [Sector(number: 32, color: .red),
+                             Sector(number: 15, color: .black),
+                             Sector(number: 19, color: .red),
+                             Sector(number: 4, color: .black),
+                             Sector(number: 21, color: .red),
+                             Sector(number: 2, color: .black),
+                             Sector(number: 25, color: .red),
+                             Sector(number: 17, color: .black),
+                             Sector(number: 34, color: .red),
+                             Sector(number: 6, color: .black),
+                             Sector(number: 27, color: .red),
+                             Sector(number: 13, color: .black),
+                             Sector(number: 36, color: .red),
+                             Sector(number: 11, color: .black),
+                             Sector(number: 30, color: .red),
+                             Sector(number: 8, color: .black),
+                             Sector(number: 23, color: .red),
+                             Sector(number: 10, color: .black),
+                             Sector(number: 5, color: .red),
+                             Sector(number: 24, color: .black),
+                             Sector(number: 16, color: .red),
+                             Sector(number: 33, color: .black),
+                             Sector(number: 1, color: .red),
+                             Sector(number: 20, color: .black),
+                             Sector(number: 14, color: .red),
+                             Sector(number: 31, color: .black),
+                             Sector(number: 9, color: .red),
+                             Sector(number: 22, color: .black),
+                             Sector(number: 18, color: .red),
+                             Sector(number: 29, color: .black),
+                             Sector(number: 7, color: .red),
+                             Sector(number: 28, color: .black),
+                             Sector(number: 12, color: .red),
+                             Sector(number: 35, color: .black),
+                             Sector(number: 3, color: .red),
+                             Sector(number: 26, color: .black),
+                             Sector(number: 0, color: .green)]
+    var spinAnimation: Animation {
+        Animation.easeOut(duration: 3.0)
+            .repeatCount(1, autoreverses: false)
+    }
+    
     func getAngle(angle: Double) -> Double {
         let deg = 360 - angle.truncatingRemainder(dividingBy: 360)
         return deg
     }
-
-    func numberFromAngle(angle: Double) -> String {
+    
+    func sectorFromAngle(angle: Double) -> String {
         var i = 0
-        var sector: String = ""
+        var sector: Sector = Sector(number: -1, color: .empty)
         
-        repeat {
+        while sector == Sector(number: -1, color: .empty) && i < sectors.count {
             let start: Double = halfSector * Double((i*2 + 1)) - halfSector
             let end: Double = halfSector * Double((i*2 + 3))
             
-            if(angle >= start && angle <= end) {
+            if(angle >= start && angle < end) {
                 sector = sectors[i]
             }
             i+=1
-        } while sector == "" && i < sectors.count
-        return "Sector\n\(sector)"
+        }
+        return "Sector\n\(sector.number) \(sector.color.rawValue)"
     }
+    
     var body: some View {
         VStack {
-            Text(self.isAnimating ? "Spining\n..." : numberFromAngle(angle: newAngle)).multilineTextAlignment(.center)
-            Image("Arrow").resizable().scaledToFit().frame(width: 20, height: 20, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-        Image("roulette").resizable().scaledToFit().rotationEffect(Angle(degrees: degreesAnimation)).frame(width: 245, height: 245, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-            .animation(foreverAnimation)
-            .onAppear { self.isAnimating = false }
+            Text(self.isAnimating ? "Spining\n..." : sectorFromAngle(angle : newAngle))
+                .multilineTextAlignment(.center)
+            Image("Arrow")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 25, height: 25, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            Image("roulette")
+                .resizable()
+                .scaledToFit()
+                .rotationEffect(Angle(degrees: spinDegrees))
+                .frame(width: 245, height: 245, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                .animation(spinAnimation)
             Button("SPIN") {
-                if(isAnimating) {
+                isAnimating = true
+                rand = Double.random(in: 1...360)
+                spinDegrees += 720.0 + rand
+                newAngle = getAngle(angle: spinDegrees)
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2.9) {
                     isAnimating = false
-                } else {
-                    isAnimating = true
-                    rand = Double.random(in: 1...360)
-                    degreesAnimation += 720.0 + rand
-                    newAngle = getAngle(angle: degreesAnimation)
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 2.9) {
-                               isAnimating = false
-                           }
                 }
-            }.padding(40)
+            }
+            .padding(40)
+            .disabled(isAnimating == true)
         }
     }
 }
